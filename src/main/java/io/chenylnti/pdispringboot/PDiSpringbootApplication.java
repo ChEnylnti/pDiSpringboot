f628201bc87d842d7f6d2252538502b4149a522a
chenylnti <2374957923@qq.com>
2021-09-29 21:39:21 +0800

    Started

---
+++ src/main/java/io/chenylnti/pdispringboot/PDiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.chenylnti.pdispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PDiSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/chenylnti/pdispringboot/PDiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chenylnti.pdispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PDiSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
